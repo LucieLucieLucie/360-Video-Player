8eda71b7c5a475c1d5909a1fec2b8b1934a9b7a4
Lucie <kruse@informatik.uni-hamburg.de>
2023-03-27 16:22:31 +1100

    removed duplicate videos / cleanup / added oculus controller

---
+++ build_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,77 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void MySnapTurn::Start()
+extern void MySnapTurn_Start_mFF9AD10A401F0336131F21DC61E13973691FCBCA (void);
+// 0x00000002 System.Void MySnapTurn::Update()
+extern void MySnapTurn_Update_m7AE7A56D07969D6E7FCEF31BD0799944FF2E3C00 (void);
+// 0x00000003 System.Void MySnapTurn::.ctor()
+extern void MySnapTurn__ctor_m03C22EEFB2776A351FC7BC440C7C0A51773FF9C5 (void);
+// 0x00000004 System.Void VideoController::Start()
+extern void VideoController_Start_m61AD2CF792C1B5615D6837402CF12DE7F9587842 (void);
+// 0x00000005 System.Void VideoController::Update()
+extern void VideoController_Update_mFC5171B82B3810F717DC3DB1CA0879B1A642F542 (void);
+// 0x00000006 System.Void VideoController::ShuffleList(System.String[])
+extern void VideoController_ShuffleList_m85EAB5E530B3D0DA3ABAE981BDFC6ACC1B4B8037 (void);
+// 0x00000007 System.Void VideoController::.ctor()
+extern void VideoController__ctor_m103EB9AC18E394EF526FBFAD32074E59551F3F06 (void);
+// 0x00000008 System.Void Unity.Template.VR.XRPlatformControllerSetup::Start()
+extern void XRPlatformControllerSetup_Start_m1F22FCA29DFD83DC0E343C3F391D04A7C52085BF (void);
+// 0x00000009 System.Void Unity.Template.VR.XRPlatformControllerSetup::.ctor()
+extern void XRPlatformControllerSetup__ctor_mF9A3998AF90962CF8F35BAF2221558BDF5F6596E (void);
+static Il2CppMethodPointer s_methodPointers[9] = 
+{
+	MySnapTurn_Start_mFF9AD10A401F0336131F21DC61E13973691FCBCA,
+	MySnapTurn_Update_m7AE7A56D07969D6E7FCEF31BD0799944FF2E3C00,
+	MySnapTurn__ctor_m03C22EEFB2776A351FC7BC440C7C0A51773FF9C5,
+	VideoController_Start_m61AD2CF792C1B5615D6837402CF12DE7F9587842,
+	VideoController_Update_mFC5171B82B3810F717DC3DB1CA0879B1A642F542,
+	VideoController_ShuffleList_m85EAB5E530B3D0DA3ABAE981BDFC6ACC1B4B8037,
+	VideoController__ctor_m103EB9AC18E394EF526FBFAD32074E59551F3F06,
+	XRPlatformControllerSetup_Start_m1F22FCA29DFD83DC0E343C3F391D04A7C52085BF,
+	XRPlatformControllerSetup__ctor_mF9A3998AF90962CF8F35BAF2221558BDF5F6596E,
+};
+static const int32_t s_InvokerIndices[9] = 
+{
+	4320,
+	4320,
+	4320,
+	4320,
+	4320,
+	3456,
+	4320,
+	4320,
+	4320,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	9,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
